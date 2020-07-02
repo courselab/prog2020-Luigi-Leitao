cf89ffacce9c05be261cd99c3009edc704828aee
Luigi Leitao <luigi.lei.tao@gmail.com>
2020-07-02 19:39:10 +0000

    update

--- m007.c
+++ m007.c
@@ -28,9 +28,69 @@
 
 int lettercount (char *s)
 {
-  return 0;
-}
+int i;
+int count;
+count = 0;
 
+for(i=0;i<MAX;i++)
+{
+switch (s[i])
+case  'a':
+case  'b':
+case  'c':
+case  'd':
+case  'e':
+case  'f':
+case  'g':
+case  'h':
+case  'i':
+case  'j':
+case  'k':
+case  'l':
+case  'm':
+case  'n':
+case  'o':
+case  'p':
+case  'q':
+case  'r':
+case  's':
+case  't':
+case  'u':
+case  'v':
+case  'w':
+case  'x':
+case  'y':
+case  'z':
+case  'A':
+case  'B':
+case  'C':
+case  'D':
+case  'E':
+case  'F':
+case  'G':
+case  'H':
+case  'I':
+case  'J':
+case  'K':
+case  'L':
+case  'M':
+case  'N':
+case  'O':
+case  'P':
+case  'Q':
+case  'R':
+case  'S':
+case  'T':
+case  'U':
+case  'V':
+case  'W':
+case  'X':
+case  'Z':
+count = count + 1;
+break; 
+}
+return count;
+}
 
 
 /* Do not edit this function. */
